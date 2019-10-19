5632b5632ec8a473949214d341ab2f0b6fad7ee0
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-19 17:24:23 -0400

    Pushing friends

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [{
+    name: "Michael Scott",
+    photo: "https://i.imgur.com/0hj9gO1.gif",
+    scores: [
+        "3",
+        "3",
+        "3",
+        "4",
+        "5",
+        "3",
+        "2",
+        "1",
+        "5",
+        "1"
+    ]
+},
+{
+    name: "Dwight Schrute",
+    photo: "https://i.imgur.com/hrBwAFq.gif",
+    scores: [
+        "5",
+        "4",
+        "2",
+        "1",
+        "1",
+        "4",
+        "5",
+        "5",
+        "4",
+        "2"
+    ]
+},
+{
+    name: "Jim Halpert",
+    photo: "https://i.imgur.com/rNgQBFI.gif",
+    scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+    ]
+},
+{
+    name: "Kevin Malone",
+    photo: "https://i.imgur.com/vVZKZSH.gif",
+    scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+    ]
+},
+{
+    name: "Gabe",
+    photo: "https://i.imgur.com/SaAAVWH.gif",
+    scores: [
+        "4",
+        "2",
+        "1",
+        "5",
+        "5",
+        "1",
+        "3",
+        "4",
+        "1",
+        "4"
+    ]
+},
+{
+    name: "Oscar Martinez",
+    photo: "https://i.imgur.com/xf5bZgL.gif",
+    scores: [
+        "2",
+        "1",
+        "5",
+        "5",
+        "4",
+        "5",
+        "2",
+        "1",
+        "5",
+        "5"
+    ]
+}
+
+];
+
+module.exports = friends;
