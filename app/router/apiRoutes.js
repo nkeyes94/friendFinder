1ec2f0ca3f339c95bc38dfa4a9be51296162a624
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-19 20:41:13 -0400

    Matching is working -- but results are limited to console

--- app/router/apiRoutes.js
+++ app/router/apiRoutes.js
@@ -7,29 +7,35 @@ module.exports = function(app) {
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
-        console.log("IN CALL")
         var bestMatch = {
             name: "",
             photo: "",
             friendDifference: Infinity
         };
-        
-        console.log(req.body.scores);
-        var userScore = req.body.scores;
-
-        for (var i = 0; i < friends.length; i++) {
-            var currentFriend = friends[i];
+        console.log("IN CALL")
+        console.log(req.body.score);
+        console.log(friends);
+        var userScore = req.body.score;
+        var totalDifference;
+        for(let i = 0; i < friends.length; i++){
+            var curFriend = friends[i];
+            console.log(curFriend);
+            console.log(userScore.length);
             totalDifference = 0;
-
             for(let x = 0; x < userScore.length; x++){
-                var currentFriendScore = friends.scores[x];
-                var currentUserScore = userScore[x];
-
-                var difference = currentFriendScore - currentUserScore;
-                console.log(difference);
+                var curFrScore = curFriend.scores[x];
+                var curUsScore = userScore[x];
+                console.log(curFrScore);
+                console.log(curUsScore);
+                totalDifference += Math.abs(parseInt(curUsScore) - parseInt(curFrScore));
             }
 
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = curFriend.name;
+                bestMatch.photo = curFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+            console.log(bestMatch.name);
         }
-        res.json(bestMatch);
     });
 };
